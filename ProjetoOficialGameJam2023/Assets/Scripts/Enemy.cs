089d4c5850efd5a26be2b7ca05d7518230a1998b
Caetano Costa <caetanogonca@gmail.com>
2023-05-23 14:35:52 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ ProjetoOficialGameJam2023/Assets/Scripts/Enemy.cs
@@ -0,0 +1,171 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private RaycastHit2D rightWall; //verificador da parede direita
+    private RaycastHit2D leftWall; //verificador da parede esquerda
+    private RaycastHit2D rigthEdge; //verificador da borda do chao direito
+    private RaycastHit2D leftEdge; //verificador da borda do chao esquerdo
+
+    public bool facingLeft = false; // verificador da posicao do sprite
+    private bool canMove = true; //Verificar se pode se mover
+    private bool recovering; // verificar se pode tomar dano ou nao
+    private float recoveringCounter; // contador ate ser possivel tomar dano
+    SpriteRenderer spriteRenderer;
+
+    [Header("Raycast")]
+    public Vector2 offset;
+    public LayerMask wallLayer;
+    public LayerMask groundLayer;
+
+    [Header("Componentes")]
+    public Rigidbody2D enemyRb;
+    public Animator enemyAnim;
+
+    [Header("Movimento")]
+    public float moveSpeed; //velocidade de movimentacao
+
+    [Header("Combate")]
+    public int health;
+    public Transform healthBar; //Barra de vida verde
+    public GameObject healthBarObject; //Objeto pai das barras
+    public float recoveryTime;
+
+    private Vector3 healthBarScale; // Tamanho da barra
+    private float healthPercent; // Percentual de vida para o calculo do tamanho da barra
+
+
+    private int direction = 1; // 1 - direita / -1 esquerda
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBarScale = healthBar.localScale;
+        healthPercent = healthBarScale.x / health;
+        //healthBarObject.SetActive(false);
+    }
+
+    void UpdateHealthBar()
+    {
+        healthBarScale.x = healthPercent * health;
+        healthBar.localScale = healthBarScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkSurroundings();
+
+        if(recovering)
+        {
+            recoveringCounter += Time.deltaTime;
+            if(recoveringCounter >= recoveryTime)
+            {
+                recoveringCounter = 0;
+                recovering = false;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (canMove)
+        {
+            Move();
+        }
+       
+    }
+    private void Move() //movimentacao
+    {
+        enemyRb.velocity = new Vector2 (moveSpeed * direction, enemyRb.velocity.y);
+    }
+
+    void Flip() //virar o sprite
+    {
+        facingLeft = !facingLeft;
+        transform.Rotate(0f, 180f, 0f);
+    }
+
+    private void checkSurroundings() //verificar paredes e chao
+    {
+        rightWall = Physics2D.Raycast(new Vector2(transform.position.x + offset.x, transform.position.y + offset.y), Vector2.right, 1f, wallLayer);
+        Debug.DrawRay(new Vector2(transform.position.x + offset.x, transform.position.y + offset.y), Vector2.right, Color.yellow);
+        if (rightWall.collider != null)
+        {
+            direction = -1;
+        }
+
+        leftWall = Physics2D.Raycast(new Vector2(transform.position.x - offset.x, transform.position.y + offset.y), Vector2.left, 1f, wallLayer);
+        Debug.DrawRay(new Vector2(transform.position.x - offset.x, transform.position.y + offset.y), Vector2.left, Color.yellow);
+        if (leftWall.collider != null)
+        {
+            direction = 1;
+        }
+
+        rigthEdge = Physics2D.Raycast(new Vector2(transform.position.x + offset.x, transform.position.y + offset.y), Vector2.down, 1f, groundLayer);
+        Debug.DrawRay(new Vector2(transform.position.x + offset.x, transform.position.y + offset.y), Vector2.down, Color.red);
+        if (rigthEdge.collider == null)
+        {
+            direction = -1;
+        }
+
+        leftEdge = Physics2D.Raycast(new Vector2(transform.position.x - offset.x, transform.position.y + offset.y), Vector2.down, 1f, groundLayer);
+        Debug.DrawRay(new Vector2(transform.position.x - offset.x, transform.position.y + offset.y), Vector2.down, Color.red);
+        if (leftEdge.collider == null)
+        {
+            direction = 1;
+        }
+
+        if(facingLeft && direction == 1 || !facingLeft && direction == -1) 
+        {
+            Flip();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<playerController>().TakeDamage(2);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (!recovering)
+        {
+            //healthBarObject.SetActive(true);
+
+            enemyAnim.SetTrigger("hurt");
+
+            health -= damage;
+
+            UpdateHealthBar();
+
+            StartCoroutine("StopMove");
+
+            recovering = true;
+
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    IEnumerator StopMove()
+    {
+        enemyRb.velocity = Vector2.zero;
+
+        canMove = false;
+
+        yield return new WaitForSeconds(.5f);
+
+        //healthBarObject.SetActive(true);
+
+        canMove = true;
+    }
+}
--- ProjetoOficialGameJam2023/Assets/Scripts/playerController.cs
+++ ProjetoOficialGameJam2023/Assets/Scripts/playerController.cs
@@ -6,18 +6,39 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
-    [SerializeField] float speed; // velocidade de movimento
+    [SerializeField] float speed;       // velocidade de movimento
     SpriteRenderer spriteRenderer;
-    Rigidbody2D rb;
-    public float forca = 5; // forca do pulo
-    public bool isGround; //esta no chao
-    public int numeroPulos = 2; // quantidade de pulos
+    private Rigidbody2D rb;
+    private Animator playerAnim;
+
+    private float recoveryCounter;      // Calculo do tempo de recuperacao
+    private float attackTimer;          // calculo do cooldown de ataque
+
+    [Header("Parametros Player")]
+    public int health;                  // Vida
+    public float forca = 5;             // forca do pulo
+    public int numeroPulos = 2;         // quantidade de pulos
+    public float recoveryTime;          // Cooldown de recuperacao
+
+    [Header("Combate")]
+    public int attackDamage;            // Dano por ataque
+    public Transform attackHit;         // Ponto de origem do ataque
+    public float attackRange;           // alcance do ataque
+    public float attackCooldown;        // cooldown do ataque
+    public LayerMask enemyLayers;       // Layers do inimigo
+
+    public bool isGround;               // esta no chao
+    private bool facingRight = false;    // olhando para direita/esquerda
+    private int facingDirection = -1;    // 1 direita / -1 esquerda
+    private bool recovering;            // Esta se recuperando de um ataque
+    private bool canMove = true;        // Permite/bloqueia a movimentacao
 
     public Animator anim;
     
     // Start is called before the first frame update
     void Start()
     {
+        playerAnim = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
     }
@@ -25,12 +46,27 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Move();
+        if(canMove)
+        {
+            Controls();
+            
+        }
+        
+
+        if(recovering) // cooldown de recuperacao
+        {
+            recoveryCounter += Time.deltaTime;
+            if(recoveryCounter >= recoveryTime)
+            {
+                recoveryCounter = 0;
+                recovering = false;
+            }
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Bloco") //verificar se o personagem esta no chao
+        if (collision.gameObject.tag == "ground") //verificar se o personagem esta no chao
         {
             isGround = true;
             numeroPulos = 2; //resetar quantidade de pulos
@@ -41,8 +77,44 @@ public class playerController : MonoBehaviour
         }
     }
 
-    private void Move()
+    private void Attack()
     {
+        anim.SetBool("Attack", true);
+
+        Collider2D[] targets = Physics2D.OverlapCircleAll(attackHit.position, attackRange, enemyLayers);
+
+        foreach (Collider2D target in targets)
+        {
+            target.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+        rb.velocity = Vector2.zero;
+        StartCoroutine("Freeze");
+
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackHit.position, attackRange);
+    }
+
+    private void Controls()
+    {
+        if (attackTimer <= 0) // se o ataque nao esta em cooldown
+        {
+            if(Input.GetKeyDown(KeyCode.X) && isGround)
+            {
+                Attack();
+                attackTimer = attackCooldown;
+            }
+        }
+        else
+        {
+            anim.SetBool("Attack", false);
+            attackTimer -= Time.deltaTime;
+        }
+
         if (Input.GetKeyDown(KeyCode.UpArrow) && numeroPulos > 0) //Pular
         {
             rb.AddForce(transform.up * forca);
@@ -52,32 +124,78 @@ public class playerController : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow)) //andar pra direita
         {
             transform.position += new Vector3(1 * speed * Time.deltaTime, 0, 0);
-            spriteRenderer.flipX = false;
+            if (!facingRight)
+            {
+                Flip();
+            }
         }
 
         if (Input.GetKey(KeyCode.LeftArrow)) //andar pra esquerda
         {
             transform.position -= new Vector3(1 * speed * Time.deltaTime, 0, 0);
-            spriteRenderer.flipX = true;
+            if(facingRight)
+            {
+                Flip();
+            }   
         }
 
-        if (Input.GetKeyDown(KeyCode.RightArrow)) //comecar animacao de andar pra direita
-        {
-            anim.SetBool("isMovingRight", true);
-        }
-        if (Input.GetKeyUp(KeyCode.RightArrow)) //terminar animacao de andar pra direita
-        {
-            anim.SetBool("isMovingRight", false);
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftArrow)) //comecar animacao de andar pra esquerda
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.LeftArrow)) //comecar animacao de andar pra direita
         {
             anim.SetBool("isMoving", true);
         }
-        if (Input.GetKeyUp(KeyCode.LeftArrow)) //terminar animacao de andar pra esquerda
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow)) //terminar animacao de andar pra direita
         {
             anim.SetBool("isMoving", false);
         }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
+        facingDirection *= -1;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if(!recovering) 
+        {
+            playerAnim.SetTrigger("hurt");
+            Knockback();
+            //vCam.GetComponent<CameraController>().CameraShake();
+            health -= damage;
+
+            recovering = true;
+
+            if (health <= 0)
+            {
+                Die();
+            }
+        }
+    }
+
+    void Die()
+    {
+        // Tocar animacao
+        anim.SetBool("isDead", true);
+    }
+
+    void Knockback()
+    {
+        rb.AddForce(new Vector2(10 * -facingDirection, 10), ForceMode2D.Impulse);
+
+        StartCoroutine("Freeze");
+    }
+
+    IEnumerator Freeze()
+    {
+        // Retira o controle do personagem
+        canMove = false;
+
+        yield return new WaitForSeconds(.5f);
+
+        // Devolve o controle do personagem
+        canMove = true;
     }
 
 
