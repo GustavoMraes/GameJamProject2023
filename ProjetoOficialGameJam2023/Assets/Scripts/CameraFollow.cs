d1318418cfef6484f9b28bf90aa87e47db1b2e91
Gustavo Henrique de Moraes <gustavomraes104@gmail.com>
2023-05-24 00:02:06 -0300

    Começando a Fase de Imagens!

--- ProjetoOficialGameJam2023/Assets/Scripts/CameraFollow.cs
+++ ProjetoOficialGameJam2023/Assets/Scripts/CameraFollow.cs
@@ -14,8 +14,10 @@ public class CameraFollow : MonoBehaviour
     private void FixedUpdate() //fazer a camera centralizar no personagem
     {
         Vector3 newPosition = player.position + new Vector3(0, 0, -5);
-        newPosition.y = 0.1f;
+        //newPosition.y = 0.1f;
         newPosition = Vector3.Lerp(transform.position, newPosition, timeLerp);
+
+
 
         transform.position = newPosition;
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, minX, maxX), transform.position.y, transform.position.z);
