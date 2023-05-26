fd636880da2707ede8dded8d1fb5563d37258cb2
Caetano Costa <caetanogonca@gmail.com>
2023-05-25 22:01:12 -0300

    aaaaaaaaaa

--- ProjetoOficialGameJam2023/Assets/Scripts/playerController.cs
+++ ProjetoOficialGameJam2023/Assets/Scripts/playerController.cs
@@ -160,6 +160,10 @@ public class playerController : MonoBehaviour
                     {
                         Attack();
                     }
+                    else
+                    {
+                        Attack();
+                    }
                 }
                
                 attackTimer = attackCooldown;
